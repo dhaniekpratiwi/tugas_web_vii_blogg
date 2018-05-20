2bb350fb27e43a8cfa820f56db37c9058de0f710
faizataniesamuftiha <39269910+faizataniesamuftiha@users.noreply.github.com>
2018-05-20 15:45:18 +0700

    Add files via upload

---
+++ act_login.php
@@ -0,0 +1,38 @@
+<?php
+//cetak dan ambil data dr database
+include 'koneksi.php';
+
+//persiapan variabel
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM akun WHERE username='".$username."'";
+$select = mysqli_query($koneksi,$query);
+
+if ($akun = mysqli_fetch_assoc($select)) {
+//ambil password di databse
+  $password_db=$akun['password'];
+  // cek enkripsi hashing multi layer
+  //1. hash md5
+  $hash_md5=md5($password);
+  //2. cek hahs bcrypt
+  $cek_bcrypt = password_verify($hash_md5,$password_db);
+
+  if ($cek_bcrypt) {
+    // simpan data ke session
+    session_start();
+    $_SESSION['id'] = $akun['id'];
+    $_SESSION['username'] = $akun['username'];
+    //arahkan ke halaman index
+    header('location:index.php');
+
+  } else {
+    echo "<h3>maaf password tidak cocok untuk akun ".$username."</h3><br>";
+    echo '<a href="login.php">Kembali</a>';
+  }
+
+} else {
+  echo "<h3>Maaf kami tdk menemukan username ".$username." dalam database </h3><br>";
+  echo '<a href="login.php">Kembali</a>';
+}
+ ?>
---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body>
+
+    <form action="act_login.php" method="post" class="form_login">
+      <table align="center" ID="tabel_login" >
+        <tr>
+          <td colspan="3">
+            <h1 align="center"> LOGIN</h1> <hr>
+          </td>
+        </tr>
+        <tr>
+          <td>Username</td>
+          <td>:</td>
+          <td>
+            <input type="text" name="username" value="">
+          </td>
+        </tr>
+        <tr>
+          <td>Password</td>
+          <td>:</td>
+          <td>
+            <input type="password" name="password" value="">
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td colspan="2" >
+            <button type="submit" class="btn_login">Login</button>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <h4>Belum punya akun? klik->
+              <a href="daftar.php" class="tulis">Daftar</a>
+            </h4>
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
